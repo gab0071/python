49ac9f652720cb69e5fc2888060fc61fe726ffab
gab0071 <jeicarm7@gmail.com>
2022-11-02 19:05:15 -0500

    operadores

---
+++ operadores/operadores.py
@@ -0,0 +1,31 @@
+# Operadores aritmeticos 
+num_uno = 80
+num_dos = 4  
+
+print('*************** ✨ CALCULADORA ✨ **************')
+print(f'la resta es: {num_uno - num_dos}')
+print(f'la suma es: {num_uno + num_dos}')
+print(f'la multiplicacion es: {num_uno * num_dos}')
+print(f'la division es: {num_uno / num_dos}')
+print(f'el resto es: {num_uno % num_dos}')
+
+
+# Operadores de asignacion 
+
+edad = 50
+edad += 5
+edad -= 10
+
+print('*** ✨ Operadores de asignacion ✨ ***')
+print(edad)
+
+# Operadores de incremento y decremento 
+year = 2022
+
+year += 1
+print(year)
+
+year -= 1
+print(year)
+
+
