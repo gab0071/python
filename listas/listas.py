ba00c9677743b4729e8d59100a11218607639aaa
catherinee24 <catherinemendez24@gmail.com>
2022-11-08 19:13:43 -0500

    listas en python

---
+++ listas/listas.py
@@ -0,0 +1,45 @@
+""" Listas en python es una coleccion o conjunto de datos o valores bajo un unico nombre.
+"""
+
+# Definiendo una lista
+peliculas = ["batman", "iron-man", "spiderman"]
+
+# Cuando le pasamos la tupla a la funcion list automaticamente se convierte en una lista
+cantantes = list(("eminem", "justin bieber", "rihanna"))
+years = list(range(2020, 2024))
+varios = ["Catellatech", 2022, False, "Cate&Gabi"]
+
+''' 
+print(peliculas)
+print(cantantes)
+print(years)
+print(varios) 
+'''
+
+#modificando la lista 
+cantantes[0] = "drake"
+print(cantantes)
+
+# Indices de las listas 
+print(peliculas[1])
+print(peliculas[-2])
+print(cantantes[0:3])
+
+# Añadir elementos a la listas 
+cantantes.append("coldplay")
+print(cantantes)
+
+# Recorrer una lista 
+nueva_pelicula = ""
+
+while nueva_pelicula != "parar":
+    nueva_pelicula = input("Introduce una peli: ")
+
+    if nueva_pelicula != "parar":
+        peliculas.append(nueva_pelicula)
+
+print("\n****** LISTADO PELICULAS ********")
+
+for movies in peliculas:
+    print(f"{peliculas.index(movies)+1}. {movies}")
+
